efd557fdf1460fe03fa8e89aedaa0b79a8274021
pablo M L <crapula8@hotmail.com>
2019-05-23 19:46:09 +0200

    interfaz grafica acabada

---
+++ JustEat/Controllers/OrderViewController.swift
@@ -0,0 +1,36 @@
+//
+//  OrderViewController.swift
+//  JustEat
+//
+//  Created by admin on 23/05/2019.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var orderLabel: UILabel!
+    
+    @IBOutlet weak var costLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
